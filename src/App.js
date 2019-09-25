a1b1c1751adfa5f590db4e5f488e4d7b119d4058
Brian Moir <brianmoir@Brians-MacBook-Pro.local>
2019-09-25 19:18:30 -0400

    trying to fix file source

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
-import FetchImg from ""
+import fetchImg from "components/fetchImg";
+import Header from "components/Header";
 
 function App() {
+  const [apodPic, setApodPic] = useState({});
+    useEffect(() =>{
+      axios.get("https://api.nasa.gov/planetary/apod")
+      .then(response =>{
+        setApodPic(response.data);
+      })
+      .catch(error=>{
+        console.log("error", error);
+      });
+    }, []);
+
+
   return (
-    <div className="App">
-      <p>
-        
-      </p>
-    </div>
+    <>
+    <Header />
+    <fetchImg
+      title={apodPic.title}
+      date={apodPic.date}
+      url={apodPic.url}
+      explanation={apodPic.explanation}
+      copyright={apodPic.copyright}
+      />
+      </>
+
   );
+  
 }
 
 export default App;
+  // <div className="App">
+    //   <p>
+        
+    //   </p>
+    // </div>
+  // );
